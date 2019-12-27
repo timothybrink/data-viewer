136b6892c04c5e1e85dbb1c887d737b32c3d8fd2
Timothy Brink <contact@timothybrink.dev>
2019-12-27 15:16:01 -0500

    add Connection class

---
+++ Connection.js
@@ -0,0 +1,21 @@
+module.exports = class Connection {
+  static generateId() {
+    return Math.round(Math.random() * 10000)
+  }
+
+  constructor(id, headers) {
+    this.id = id
+    this.headers = headers
+    this.data = []
+    this.finished = false
+  }
+
+  update(data) {
+    this.data.push(data)
+    console.log(data)
+  }
+
+  close() {
+    this.finished = true
+  }
+}
