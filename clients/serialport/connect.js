85335dbeaa1bfbc31cfef8fea4d46365310e4c43
Timothy Brink <contact@timothybrink.dev>
2020-02-13 13:05:17 -0500

    improve cli

--- clients/serialport/connect.js
+++ clients/serialport/connect.js
@@ -1,8 +1,41 @@
 const SerialPortTelemetry = require('./SerialPortTelemetry')
+const yargs = require('yargs')
 
-let port = process.argv[2]
-let spt = new SerialPortTelemetry(port)
-if (process.argv[3]) {
-  spt.baudRate = Number(process.argv[3])
-}
+yargs
+  .option('port', {
+    alias: 'p',
+    type: 'string',
+    description: 'Serial port to connect to'
+  })
+  .option('sep', {
+    alias: 's',
+    type: 'string',
+    description: 'Data separator to use (e.g. \',\' or \'|\')'
+  })
+  .option('br', {
+    alias: 'b',
+    type: 'number',
+    description: 'Baud rate to connect with'
+  })
+  .option('headers', {
+    alias: 'h',
+    type: 'array',
+    description: 'Names for the data columns (in order)'
+  })
+  .option('server', {
+    type: 'string',
+    description: 'Telemetry server to connect to'
+  })
+
+argv = yargs.argv
+
+console.log('Port:', argv.port)
+console.log('Separator:', argv.sep || ',')
+console.log('Baud rate:', argv.br)
+console.log('Headers:', argv.headers)
+console.log('Server:', argv.server)
+
+let spt = new SerialPortTelemetry(argv.port, argv.br, argv.server)
+spt.data.seperator = argv.seperator
+spt.setHeaders(argv.headers)
 spt.init()
