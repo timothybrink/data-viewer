a7466a7464fcf6f87f1c327ddc2256bbb3d68e4e
Timothy Brink <contact@timothybrink.dev>
2020-02-13 18:33:02 -0500

    remove try/catch block

--- ui/main.js
+++ ui/main.js
@@ -34,27 +34,23 @@ function initWebSocket(retry) {
 
   ws.addEventListener('message', function (event) {
     // We got something from the server:
-    try {
-      data = JSON.parse(event.data)
-      // Not data, just a message telling us the server has a new data source
-      if (data.event == 'data-opened') {
-        console.log('Headers received:', data.headers)
-        dataMgr.initDataStream(data.id, data.headers)
-        dataConnectionIds.push(data.id)
-      }
-      // Not data either, a message telling us that a connection to the
-      // server was closed
-      else if (data.event == 'data-closed') {
-        console.log('Data stream', data.id, 'closed.')
-        dataMgr.closeDataStream(data.id)
-        dataConnectionIds.splice(dataConnectionIds.indexOf(data.id), 1)
-      }
-      // Data was recieved from the source with the given id.
-      else if (data.data) {
-        dataMgr.newData(data.id, data.time, data.data)
-      }
-    } catch (e) {
-      console.error(e)
+    data = JSON.parse(event.data)
+    // Not data, just a message telling us the server has a new data source
+    if (data.event == 'data-opened') {
+      console.log('Headers received:', data.headers)
+      dataMgr.initDataStream(data.id, data.headers)
+      dataConnectionIds.push(data.id)
+    }
+    // Not data either, a message telling us that a connection to the
+    // server was closed
+    else if (data.event == 'data-closed') {
+      console.log('Data stream', data.id, 'closed.')
+      dataMgr.closeDataStream(data.id)
+      dataConnectionIds.splice(dataConnectionIds.indexOf(data.id), 1)
+    }
+    // Data was recieved from the source with the given id.
+    else if (data.data) {
+      dataMgr.newData(data.id, data.time, data.data)
     }
   })
 
